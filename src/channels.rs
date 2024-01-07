b436825241c0a0fc05c7879980dc811ac1e65dc0
Andrew-LC <smartandrew2003@gmail.com>
2024-01-07 22:55:18 +0530

    implemented channels

--- src/channels.rs
+++ src/channels.rs
@@ -1,17 +1,24 @@
 use bytes::Bytes;
 use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 use mini_redis::client;
 
 #[derive(Debug)]
 enum Command {
     Get {
-	key: String,
+        key: String,
+        resp: Responder<Option<Bytes>>,
     },
     Set {
-	key: String,
-	val: Bytes,
-    }
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
 }
+
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 
 #[tokio::main]
@@ -28,11 +35,14 @@ async fn main() {
             use Command::*;
 
             match cmd {
-		Get { key } => {
-                    client.get(&key).await;
+		Get { key, resp } => {
+                    let res = client.get(&key).await;
+		    // Ignore errors
+		    let _ = resp.send(res);
 		}
-		Set { key, val } => {
-                    client.set(&key, val).await;
+		Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+		    let _ = resp.send(res);
 		}
             }
 	}
@@ -40,27 +50,38 @@ async fn main() {
 
     // Spawn two tasks, one gets a key, the other sets a key
     let t1 = tokio::spawn(async move {
+	let (resp_tx, resp_rx) = oneshot::channel();
 	let cmd = Command::Get {
 	    key: "foo".to_string(),
+	    resp: resp_tx,
 	};
 
+	// Send the GET request
 	tx.send(cmd).await.unwrap();
+
+	// Await the response
+	let res = resp_rx.await;
+	println!("GOT = {:?}", res);
     });
 
     let t2 = tokio::spawn(async move {
+	let (resp_tx, resp_rx) = oneshot::channel();
 	let cmd = Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            resp: resp_tx,
 	};
 
+	// Send the SET request
 	tx2.send(cmd).await.unwrap();
+
+	// Await the response
+	let res = resp_rx.await;
+	println!("GOT = {:?}", res);
     });
+
 
     t1.await.unwrap();
     t2.await.unwrap();
     manager.await.unwrap();
-
-    while let Some(message) = rx.recv().await {
-	println!("GOT = {}", message);
-    }
 }
