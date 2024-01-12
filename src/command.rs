7d5db68643e5f7c73ebe5919661baf4f1052dba3
Andrew-LC <smartandrew2003@gmail.com>
2024-01-12 23:24:52 +0530

    batman

--- src/command.rs
+++ src/command.rs
@@ -1,19 +1,12 @@
 use bytes::Bytes;
-use tokio::sync::oneshot;
 
 #[derive(Debug)]
-pub enum Command {
+enum Command {
     Get {
         key: String,
-        resp: Responder<Option<Bytes>>,
     },
     Set {
         key: String,
         val: Bytes,
-        resp: Responder<()>,
     },
 }
-
-/// Provided by the requester and used by the manager task to send
-/// the command response back to the requester.
-type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
